72a7b03e50392f7516a1a152d074a0cbb4cbbb31
AifeiI <595345150@qq.com>
2019-06-10 11:23:02 +0800

    Add API Document

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,13 @@
-## 0.0.1
+## 0.0.4
 
-* Add decoding function
+* Add API Document
+
+## 0.0.3
 
 ## 0.0.2
 
 * delete files lib/*.jar
+
+## 0.0.1
+
+* Add decoding function
--- lib/qr_code_tools.dart
+++ lib/qr_code_tools.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class QrCodeToolsPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('qr_code_tools');
+  static const MethodChannel _channel = const MethodChannel('qr_code_tools');
 
+  /// [file] is local file path
   static Future<String> from(String file) async {
     final String data = await _channel.invokeMethod('decoder', {'file': file});
     return data;
