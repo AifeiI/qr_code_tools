becf7be895d29853988d79026b0426233747c62c
Volodymyr Buberenko <v.buberenko@gmail.com>
2023-03-17 19:22:29 +0200

    Update example app, update Zxing to 3.3.3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -50,32 +50,33 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              _qrcodeFile.isEmpty
-                  ? Expanded(
-                child: Image.asset(
-                        'images/1559788943.png',
-                      ),
-                  )
-                  : Expanded(child: Image.file(File(_qrcodeFile))),
-              ElevatedButton(
-                child: Text("Select file"),
-                onPressed: _getPhotoByGallery,
+        appBar: AppBar(title: const Text('Plugin example app')),
+        body: Column(
+          children: <Widget>[
+            _qrcodeFile.isEmpty
+                ? Expanded(flex: 2, child: Image.asset('images/1559788943.png'))
+                : Expanded(flex: 2, child: Image.file(File(_qrcodeFile))),
+            Expanded(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    child: Text("Select file from gallery"),
+                    onPressed: _getImageFromGallery,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(24),
+                    child: Text('Qr Code data: $_data\n'),
+                  ),
+                ],
               ),
-              Text('Qr Code data: $_data\n'),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Future<void> _getPhotoByGallery() async {
+  Future<void> _getImageFromGallery() async {
     Stream.fromFuture(picker.pickImage(source: ImageSource.gallery))
         .flatMap((file) {
       setState(() {
